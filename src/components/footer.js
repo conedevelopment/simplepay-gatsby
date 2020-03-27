c88415226fc20cb41c90c9a9aa05ed57bdbe6acb
Adam Laki <info@adamlaki.com>
2020-03-27 17:58:54 +0100

    Add new component names

---
+++ src/components/footer.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Logo from "../assets/svgs/logo-monochrome.svg"
+import footerStyles from "./footer.module.scss"
+
+const Footer = () => {
+  return (
+    <footer className={footerStyles.footer}>
+      <div className={footerStyles.inner}>
+        <Link className={footerStyles.logo} to="/" title="Cone Development">
+          <Logo />
+        </Link>
+        <div className={footerStyles.col}>
+          <ul className={footerStyles.navigation}>
+            <li>
+              <a href="https://conedevelopment.com/hu/szolgaltatasaink/alkalmazas-fejlesztes/">Webalkalmazás fejlesztés</a>
+            </li>
+            <li>
+              <a href="https://conedevelopment.com/hu/szolgaltatasaink/weboldal-fejlesztes/">Weboldal fejlesztés</a>
+            </li>
+            <li>
+              <a href="https://conedevelopment.com/hu/rolunk/">Rólunk</a>
+            </li>
+            <li>
+              <a href="https://conedevelopment.com/hu/adatkezelesi-tajekoztato/">Adatkezelési tájékoztató</a>
+            </li>
+          </ul>
+          <p className={footerStyles.copyright}>2020 © Cone Development</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
