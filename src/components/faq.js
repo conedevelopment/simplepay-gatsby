b455f585f504561818be5a17ffa35a294f2fa4e0
Adam Laki <info@adamlaki.com>
2020-04-30 15:36:16 +0200

    Add MIT license link to FAQ

--- src/components/faq.js
+++ src/components/faq.js
@@ -10,7 +10,7 @@ const Faq = () => {
           <h2 className={faqStyles.title}>Kérdésed van?</h2>
           <div className={faqStyles.block}>
             <h3 className={faqStyles.blockTitle}>Valóban ingyenes a bővímtény?</h3>
-            <p className={faqStyles.blockDescription}>Igen, ezt a bővítményt költségek nélkül töltheted le és használhatod.</p>
+            <p className={faqStyles.blockDescription}>Igen, ezt a bővítményt <a href="https://github.com/thepinecode/simplepay-gateway/blob/master/LICENSE">MIT licensz</a> alatt költségek nélkül töltheted le és használhatod.</p>
           </div>
           <div className={faqStyles.block}>
             <h3 className={faqStyles.blockTitle}>Jár hozzá frissítés?</h3>
