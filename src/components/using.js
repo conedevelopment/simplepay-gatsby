d643c311e4b0a924a9005cfc3a925a6e48526836
D. Nagy Gergő <hello@iamgergo.com>
2021-03-08 15:08:10 +0100

    Update using.js

--- src/components/using.js
+++ src/components/using.js
@@ -33,6 +33,7 @@ const Using = () => {
             <li><a href="https://www.caleo.hu/" rel="nofollow">Caleo fűtőfilm</a></li>
             <li><a href="https://zelenaksutihaz.hu/" rel="nofollow">Zelenák Sütiház</a></li>
             <li><a href="https://szokimondoka.hu/webaruhaz/" rel="nofollow">Szókimondóka</a></li>
+            <li><a href="https://www.kaptarko.hu/" rel="nofollow">Kaptárkő Egyesület</a></li>
           </ul>
           <p className={usingStyles.info}>A fenti listát szeretnénk bővíteni. Amennyiben használod a bővítményünket, úgy megköszönjük, ha elküldöd nekünk az oldal URL-jét e-mailben a következő címre: <a href="mailto:hello@conedevelopment.com">hello@conedevelopment.com</a>.</p>
         </div>
